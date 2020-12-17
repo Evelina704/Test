ee7c7e3fc989bd3d4c5fd1aa848e336f3798a72b
nemowaymustdie <inobody16@gmail.com>
2020-12-17 23:56:35 +0600

    Task1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        int[] radiuses = new int[10];
+
+        for(int i = 0, j = 1; i < radiuses.length; i++, j++){
+            radiuses[i] = j;
+        }
+
+        int count = 0;
+        for(int i = 0; i < radiuses.length; i++){
+            Circle circle = Circle.makeCircle(radiuses[i]);
+            if(circle.getArea() % 3 == 0){
+                count++;
+                System.out.println(String.format("При радиусе %s площадь равна %s и оно кратно трём!", radiuses[i],circle.getArea()));
+            }
+        }
+
+        System.out.println(String.format("Всего %s кругов кратны трём!", count));
     }
 }
